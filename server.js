d985cdd62101d1a243dcef2bb38aaf616ebea318
younusraza909 <younusraza909@gmail.com>
2020-07-08 03:25:50 -0700

    Made Graphql Sever With Express Made All Scehma

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = require("./schema");
 
 const app = express();
 
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: MyGraphQLSchema,
-    graphql: true,
+    schema: schema,
+    graphiql: true,
   })
 );
 
-app.listen(4000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Started on port ${PORT}`);
+});
