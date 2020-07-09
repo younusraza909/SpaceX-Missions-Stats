d943248f4f695f1f9e109320c40d18b1b56345fa
younusraza909 <younusraza909@gmail.com>
2020-07-09 08:19:37 -0700

    Made Public Folder For Deployment

---
+++ public/precache-manifest.8cb0503ac4736b7395e22016e12294ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a4d31166f6217ba9b70a1318218d9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c9d1346024510d0cae",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4031c02f1df69ef6aa8e",
+    "url": "/static/js/2.17843b39.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.17843b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c9d1346024510d0cae",
+    "url": "/static/js/main.ed03ecd9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
